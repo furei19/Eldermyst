d475d9438c05be2c05064700b8fb35e4c08d92cd
yosef <115853989+furei19@users.noreply.github.com>
2023-08-22 08:02:36 -0700

    Add files via upload

---
+++ GameStart/rpgRace.js
@@ -0,0 +1,46 @@
+let rpgRace;
+
+// Race variables
+
+const human = document.querySelector("#human");
+const elf = document.querySelector("#elf");
+const dwarf = document.querySelector("#dwarf");
+const giant = document.querySelector("#giant");
+const orc = document.querySelector("#orc");
+const beast = document.querySelector("#beast");
+
+// buttons
+
+human.onclick = humanRace;
+elf.onclick = elfRace;
+dwarf.onclick = dwarfRace;
+giant.onclick = giantRace;
+orc.onclick = orcRace;
+beast.onclick = beastRace;
+
+// Functions
+
+function humanRace() {
+  rpgRace = "human";
+  console.log("human");
+}
+function elfRace() {
+  rpgRace = "elf";
+  console.log("elf");
+}
+function dwarfRace() {
+  rpgRace = "dwarf";
+  console.log("dwarf");
+}
+function giantRace() {
+  rpgRace = "giant";
+  console.log("giant");
+}
+function orcRace() {
+  rpgRace = "orc";
+  console.log("orc");
+}
+function beastRace() {
+  rpgRace = "beast";
+  console.log("beast");
+}
